144dd3ab4acf62ce447b146e51954ce5776e6f93
Eetu Mäkelä <eetu.makela@aalto.fi>
2015-12-16 14:54:37 +0200

    readd needed charset conversion

--- test/fetch-cemf.bash
+++ test/fetch-cemf.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl "http://kaino.kotus.fi/korpus/1800/meta/1800_coll_rdf.xml" | grep -a '"/korpus/1800/teksti/' | sed 's|.*"/korpus/1800/meta/\(.*\)".*|http://kaino.kotus.fi/korpus/1800/meta/\1|' | xargs wget --continue -P cemf-list
-grep -h -a kotus:class cemf-list/*.xml | sed 's|.*rdf:resource="\(.*\)" kotus:class.*|\1|' | sed 's|^|http://kaino.kotus.fi|' | xargs wget --continue -P cemf
+grep -h -a kotus:class cemf-list/*.xml | iconv -f 'iso-8859-1' -t 'utf-8' | sed 's|.*rdf:resource="\(.*\)" kotus:class.*|\1|' | sed 's|^|http://kaino.kotus.fi|' | xargs wget --continue -P cemf
 rm -rf cemf-list
 mkdir cemf-dict
 mv cemf/calamnius_aristoteleen_runousoppi1871.xml cemf-dict
