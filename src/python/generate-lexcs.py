c41df9f45136c85fad3c2f3a8f7d4c5c95ec0ebc
Eetu Mäkelä <eetu.makela@aalto.fi>
2016-02-22 14:19:17 +0200

    fix lexc generation

--- src/python/generate-lexcs.py
+++ src/python/generate-lexcs.py
@@ -111,7 +111,9 @@ def main():
     formatter = None
     if args.format == 'omor':
         formatter = OmorFormatter(args.verbose, new_para=args.omor_new_para,
-                                  allo=args.omor_allo, props=args.omor_props, sem=args.omor_sem)
+                                  allo=args.omor_allo, props=args.omor_props,
+                                  sem=args.omor_sem, segments=args.segments,
+                                  ktnkav=args.ktnkav)
     elif args.format == 'ftb3':
         formatter = Ftb3Formatter(args.verbose)
     elif args.format == 'apertium':
